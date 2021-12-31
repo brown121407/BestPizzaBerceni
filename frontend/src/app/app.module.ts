790c5ef88f0b55a470c4247f7c94fdf0cf501c66
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2021-12-31 16:51:30 +0200

    frontend: Make layout centered.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,7 +9,7 @@ import {MatButtonModule} from "@angular/material/button";
 import { LoginComponent } from './components/login/login.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { SignupComponent } from './components/signup/signup.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ToastrModule} from "ngx-toastr";
@@ -20,6 +20,8 @@ import { MenuComponent } from './components/menu/menu.component';
 import { RefreshComponent } from './components/refresh/refresh.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatChipsModule} from "@angular/material/chips";
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -54,6 +56,9 @@ export function tokenGetter() {
       }
     }),
     MatCardModule,
+    MatIconModule,
+    FormsModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
