3fe78ec1d388e12fdd7619fbe3fce2a661e66d14
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2022-01-09 22:40:47 +0200

    Add components for coupons and role guards.

---
+++ frontend/src/app/modules/coupons/components/new-coupon/new-coupon.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewCouponComponent } from './new-coupon.component';
+
+describe('NewCouponComponent', () => {
+  let component: NewCouponComponent;
+  let fixture: ComponentFixture<NewCouponComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewCouponComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewCouponComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
