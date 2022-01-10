553c11020902798bb22a68e51d785aad70623b81
AndreeaGherghescu <a_diana_gherghescu@yahoo.com>
2022-01-10 21:02:57 +0200

    Add user list.

---
+++ frontend/src/app/modules/users/components/user/user.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {IUser} from "../../../../models/user";
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class userComponent implements OnInit {
+
+  users: IUser[] = [];
+
+  constructor(private userService: UserService, private router: Router, private toastr: ToastrService) { }
+
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((res: IUser[]) => this.users = res);
+  }
+
+  goToPage(pageName: string){
+    this.router.navigate([`${pageName}`]);
+  }
+
+  dltUser(id: string) {
+    this.userService.deleteUser(id).subscribe((_) =>
+      {
+        this.toastr.success("User deleted successfully");
+        this.users = this.users.filter((us: IUser) => us.id !== id);
+      }
+    );
+  }
+
+}
